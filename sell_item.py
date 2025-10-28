9e029ad40a638cb9323885a36d4b3ae5f60dbfd9
Retesh <retesh.acad@gmail.com>
2025-10-28 16:27:55 +0530

    modified sell_item

--- sell_item.py
+++ sell_item.py
@@ -1,5 +1,4 @@
 import pymongo
-import re
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["authentication"]   
@@ -13,3 +12,19 @@ def add():
     item_discription =  input("enter the descripton: ")
     price = float(int("Enter the price: "))
     link = input("enter the image drive link: ")
+    if not drive_link.startswith("http://drive"):
+        print("Invalid link! Please paste a valid Google Drive link.")
+        return
+    else:
+        note_doc = {
+            "name": name,
+            "semester": sem,
+            "program": program,
+            "item_name": item_name,
+            "item_discription": item_discription,
+            "price": price,
+            "link": link
+        }
+    print("Item added successfully!")
+
+    
